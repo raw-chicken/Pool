49387ac1bcbc568d2e4a74045d1ec537443edfa0
Daniel Carr <randomerz56@gmail.com>
2022-10-22 02:10:38 -0400

    Fix merges

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
 import StartPage from './Pages/StartPage.js'
+import NewEventPage from './Pages/NewEventPage.js'
 import Chat from './Pages/Chat.js';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path='/' element={<StartPage />} />
+        <Route path='/new' element={<NewEventPage />} />
       </Routes>
     </Router>
   );
