97d9dfe0ce3db040264276a7c6cee824ee0878a3
Chase-rgb <62972320+Chase-rgb@users.noreply.github.com>
2022-10-22 12:29:27 -0400

    Render firebase chat with static groupID

--- src/Pages/Chat.js
+++ src/Pages/Chat.js
@@ -1,3 +1,5 @@
+import '../css/Chat.css';
+
 import React, { Component } from "react";
 import { database } from "../firebase/firebase"
 import { ref, onValue } from "firebase/database";
@@ -23,35 +25,51 @@ export default class Chat extends Component {
             // snapshot.forEach((snap) => {
             //     chats.push(snap.val());
             // });
-            const messages = ref(database, 'chats');
+            const messages = ref(database, 'chats/group1');
             onValue(messages, (snapshot) => {
-                console.log(snapshot)
-                const data = snapshot.val();
-                console.log(data)
-                this.setState({ data });
+                // let messages = [];
+                console.log(snapshot.val())
+                // snapshot.forEach((snap) => {
+                //     // console.log(snap.val())
+                //     messages.push(snap.val());
+                // })
+                this.setState( {chats: snapshot.val()} );
+                // console.log(this.state.chats)
             });
         } catch (error) {
             console.log(error)
             this.setState({ readError: error.message });
         }
     }
-    componentWillUnmount() {
-        console.log("unmount")
-    }
-    render() {
-        return (
-          <div>
-            <div className="chats">
-              {this.state.chats.map(chat => {
-                return <p key={chat.timestamp}>{chat.content}</p>
-              })}
-            </div>
-            <div>
-              Login in as:
-            </div>
-          </div>
-        );
-      }
+render() {
+    return (
+      <div>
+        <div className="chat-area">
+          {/* loading indicator */}
+          {/* {this.state.loadingChats ? <div className="spinner-border text-success" role="status">
+            <span className="sr-only">Loading...</span>
+          </div> : ""} */}
+          {/* chat area */}
+          {/* {console.log(Object.entries(this.state.chats))    } */}
+          "In render"
+          {Object.entries(this.state.chats).map(([messageKey, message]) => {
+            return <p className={"chat-bubble"} key={messageKey}>
+              {message}
+              <br />
+            </p>
+          })}
+        </div>
+        {/* <form onSubmit={this.handleSubmit} className="mx-3">
+          <textarea className="form-control" name="content" onChange={this.handleChange} value={this.state.content}></textarea>
+          {this.state.error ? <p className="text-danger">{this.state.error}</p> : null}
+          <button type="submit" className="btn btn-submit px-5 mt-4">Send</button>
+        </form>
+        <div className="py-5 mx-3">
+          Login in as: <strong className="text-info">{this.state.user.email}</strong>
+        </div> */}
+      </div>
+    );
+  }
 }
   
   
