0783e86f5ea7631177cb798f1fe23220e45975e6
Allison Lu <allison.lu@gatech.edu>
2022-10-22 02:15:38 -0400

    complete start page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import './App.css';
+import './css/App.css';
 
 import StartPage from './Pages/StartPage.js'
 import Chat from './Pages/Chat.js';
--- src/Pages/StartPage.js
+++ src/Pages/StartPage.js
@@ -1,12 +1,21 @@
 import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
+import '../css/App.css';
+import '../css/Start.css';
+import { black } from '@mui/material/colors';
 
 function StartPage() {
+    document.body.style = 'background: #77BB3F;';
+
     return (
-        <Button variant="contained" endIcon={<SendIcon />}>
-        Send
-        </Button>
+        <div className="stacked">
+            <img src = "pool-transparent.png" alt="logo"></img>
+
+            <Button variant="contained" size="large"
+            sx={{color:"black", backgroundColor:"white"}} className="btn">
+                Create New Event
+            </Button>
+        </div>
     );
   }
-  
+
 export default StartPage;
