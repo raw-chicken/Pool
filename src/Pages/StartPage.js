b8af47250cfe140b30ad85e7031b3f736e97d75c
Jason Guo <jasonyguo@gmail.com>
2022-10-22 11:40:51 -0400

    linting

--- src/Pages/StartPage.js
+++ src/Pages/StartPage.js
@@ -5,55 +5,55 @@ import { Link } from 'react-router-dom';
 import '../css/App.css';
 import '../css/Start.css';
 
-function StartPage() {
-    document.body.style = 'background: #77BB3F;';
+function StartPage(props) {
+  document.body.style = 'background: #77BB3F;';
 
-    return (
-        
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          style={{ 
-            minHeight: '100vh',
-          }}
+  return (
+    
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ 
+        minHeight: '100vh',
+      }}
+      sx={{
+        mx: 'auto'
+      }}
+    >
+      <div className="stacked">
+        <img src = "pool-transparent.png" alt="logo"></img>
+
+        <Button 
+          component={Link} 
+          to="/new" 
+          variant="contained" 
+          size="large"
           sx={{
-            mx: 'auto'
-          }}
+            color:"black", 
+            backgroundColor:"#F7F7F6",
+            borderRadius: 25,
+            ':hover': {
+              backgroundColor: '#F7F7F6',
+            }
+          }} 
+          className="btn"
         >
-            <div className="stacked">
-                <img src = "pool-transparent.png" alt="logo"></img>
-
-                <Button 
-                    component={Link} 
-                    to="/new" 
-                    variant="contained" 
-                    size="large"
-                    sx={{
-                        color:"black", 
-                        backgroundColor:"#F7F7F6",
-                        borderRadius: 25,
-                        ':hover': {
-                            backgroundColor: '#F7F7F6',
-                        }
-                    }} 
-                    className="btn"
-                >
-                    <Typography
-                        variant="h5"
-                        sx={{
-                            fontWeight: 'bold',
-                            color: '#444'
-                        }}
-                    >
-                        Start Planning
-                    </Typography>
-                </Button>
-            </div>
-        </Grid>
-    );
-  }
+          <Typography
+            variant="h5"
+            sx={{
+              fontWeight: 'bold',
+              color: '#444'
+            }}
+          >
+            Start Planning
+          </Typography>
+        </Button>
+      </div>
+    </Grid>
+  );
+}
 
 export default StartPage;
