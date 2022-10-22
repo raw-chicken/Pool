66d351cb56c9a1ebfd2c22d7db7d8da529c496e8
Jason Guo <jasonyguo@gmail.com>
2022-10-22 18:37:58 -0400

    very bad null handling

--- src/Pages/EventPage.js
+++ src/Pages/EventPage.js
@@ -35,9 +35,12 @@ class EventPage extends Component {
 
   render() {
     let count = 0;
-    const groupsDisplay = Object.keys(this.state.groups).map((group) =>
-      <Group groupID={group} key={count++}/>
-    )
+    let groupsDisplay = undefined;
+    if (this.state.groups !== undefined) {
+      groupsDisplay = Object.keys(this.state.groups).map((group) =>
+        <Group groupID={group} key={count++}/>
+      )
+    }
 
     return( 
       <div>
