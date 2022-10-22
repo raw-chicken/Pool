18ae8c2189cffd6090c68dd5cc54d89ce72b118b
Jason Guo <jasonyguo@gmail.com>
2022-10-22 14:33:43 -0400

    KMS

--- src/Pages/EventPage.js
+++ src/Pages/EventPage.js
@@ -8,47 +8,50 @@ import Button from '@mui/material/Button';
 import Group from "../Components/Group.js"
 import Header from "../Components/Header.js"
 
+import { useParams } from "react-router-dom";
+
 function EventPage() {
+  const { id } = useParams();
+  console.log("ID: ", id);
+  // const groupsDisplay = this.state.groups.map((group) =>
+  //   <Group />
+  // )
 
-    // const groupsDisplay = this.state.groups.map((group) =>
-    //   <Group />
-    // )
+  return( 
+    <div>
+    <Header/>
 
-    return( 
+    <div className='content fill-stack'>
       <div>
-        <Header/>
-
-        <div className='content fill-stack'>
-            <div>
-                <h1>Event Name</h1>
-                <p>description</p>
-            </div>
-
-            {/* {groupsDisplay} */}
-
-            {/* should add a new group bc new driver on click */}
-            <Button 
-            variant="contained" 
-            size="large"
-            sx={{
-                color:"#F7F7F6", 
-                backgroundColor:"#77BB3F",
-                borderRadius: 10,
-                ':hover': {
-                backgroundColor: '#77BB3F',
-                }
-            }} 
-            className="btn"
-            >
-            <Typography
-                variant="h7"
-            >
-                I'm a driver
-            </Typography>
-            </Button>
-        </div>
+        <h1>Event Name</h1>
+        <p>description</p>
       </div>
-    )
+
+      {/* {groupsDisplay} */}
+
+      {/* should add a new group bc new driver on click */}
+      <Button 
+      variant="contained" 
+      size="large"
+      sx={{
+        color:"#F7F7F6", 
+        backgroundColor:"#77BB3F",
+        borderRadius: 10,
+        ':hover': {
+          backgroundColor: '#77BB3F',
+        }
+      }} 
+      className="btn"
+      >
+      <Typography
+        variant="h7"
+      >
+        I'm a driver
+      </Typography>
+      </Button>
+    </div>
+    </div>
+  )
 
    // console.log(this.props.match.params.id);  
 }
