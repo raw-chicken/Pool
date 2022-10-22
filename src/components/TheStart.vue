bb6d3cee691684aeeb835009c111da36a43a4b09
Allison Lu <allison.lu@gatech.edu>
2022-10-21 23:47:12 -0400

    add new logo

--- src/components/TheStart.vue
+++ src/components/TheStart.vue
@@ -1,13 +1,21 @@
 <template>
     <div class="page">
-    <img
-      alt="Pool logo"
-      class="logo"
-      src="@/assets/pool-logo.jpeg"
-      width="300"
-      height="300"
-    />
-    <h3>Let's get started!</h3>
-    <button>--></button>
+        <img
+        alt="Pool logo"
+        class="logo"
+        src="@/assets/pool-transparent.png"
+        width="300"
+        height="300"
+        />
+        <v-btn
+  elevation="2"
+  fab
+  icon
+  large
+></v-btn>
     </div>
 </template>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createVuetify } from "vuetify";
 
 import App from "./App.vue";
 import router from "./router";
@@ -7,9 +8,10 @@ import router from "./router";
 import "./assets/main.css";
 
 const app = createApp(App);
+const vuetify = createVuetify()
 
 app.use(createPinia());
-
+app.use(vuetify)
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');
