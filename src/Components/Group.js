60dc12ef87c0c9b88505cce088280fe85ec71584
Allison Lu <allison.lu@gatech.edu>
2022-10-22 14:40:36 -0400

    change back to classes

--- src/Components/Group.js
+++ src/Components/Group.js
@@ -3,55 +3,61 @@ import { Typography } from '@mui/material';
 import React, { Component } from 'react';
 import '../css/App.css';
 
-function Group() {
-    const ridersDisplay = this.state.riders.map((rider) =>
-        <p className="item">{rider}</p>
-    );
+export default class Group extends Component {
 
-    return( 
-        <div className="box p3">
-            <h3 className="item">{this.state.driver}</h3>
-            {ridersDisplay}
-            <div className="item">
-            <Button 
-                variant="contained" 
-                size="small"
-                sx={{
-                    color:"#F7F7F6", 
-                    backgroundColor:"#77BB3F",
-                    ':hover': {
-                    backgroundColor: '#77BB3F',
-                    }
-                }} 
-                className="btn"
-                >
-                <Typography
-                    variant="h7"
-                >
-                    Edit
-                </Typography>
-            </Button>
-            <Button 
-                variant="contained" 
-                size="small"
-                sx={{
-                    color:"#F7F7F6", 
-                    backgroundColor:"#77BB3F",
-                    ':hover': {
-                    backgroundColor: '#77BB3F',
-                    }
-                }} 
-                className="btn"
-                >
-                <Typography
-                    variant="h7"
-                >
-                    Join
-                </Typography>
-            </Button>
+    constructor(props) {
+        super(props)
+        this.state = {driver: "Driver", riders:[]};
+    }
+
+    render() {
+        const ridersDisplay = this.state.riders.map((rider) =>
+            <p className="item">{rider}</p>
+        );
+
+        return( 
+            <div className="box p3">
+                <h3 className="item">{this.state.driver}</h3>
+                {ridersDisplay}
+                <div className="item">
+                <Button 
+                    variant="contained" 
+                    size="small"
+                    sx={{
+                        color:"#F7F7F6", 
+                        backgroundColor:"#77BB3F",
+                        ':hover': {
+                        backgroundColor: '#77BB3F',
+                        }
+                    }} 
+                    className="btn"
+                    >
+                    <Typography
+                        variant="h7"
+                    >
+                        Edit
+                    </Typography>
+                </Button>
+                <Button 
+                    variant="contained" 
+                    size="small"
+                    sx={{
+                        color:"#F7F7F6", 
+                        backgroundColor:"#77BB3F",
+                        ':hover': {
+                        backgroundColor: '#77BB3F',
+                        }
+                    }} 
+                    className="btn"
+                    >
+                    <Typography
+                        variant="h7"
+                    >
+                        Join
+                    </Typography>
+                </Button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
-export default Group;
--- src/Pages/EventPage.js
+++ src/Pages/EventPage.js
@@ -10,51 +10,56 @@ import Header from "../Components/Header.js"
 
 import { useParams } from "react-router-dom";
 
-function EventPage() {
-  const { id } = useParams();
-  console.log("ID: ", id);
+export default class EventPage extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {name: "Event Name", description: "description", groups:[]}
+  }
+
+  render() {
+ // const { id } = useParams();
+  //console.log("ID: ", id);
   // const groupsDisplay = this.state.groups.map((group) =>
   //   <Group />
   // )
 
   return( 
-    <div>
-    <Header/>
-
-    <div className='content fill-stack'>
       <div>
-        <h1>Event Name</h1>
-        <p>description</p>
+      <Header/>
+
+      <div className='content fill-stack'>
+        <div>
+          <h1>Event Name</h1>
+          <p>description</p>
+        </div>
+
+        {/* {groupsDisplay} */}
+
+        {/* should add a new group bc new driver on click */}
+        <Button 
+        variant="contained" 
+        size="large"
+        sx={{
+          color:"#F7F7F6", 
+          backgroundColor:"#77BB3F",
+          borderRadius: 10,
+          ':hover': {
+            backgroundColor: '#77BB3F',
+          }
+        }} 
+        className="btn"
+        >
+        <Typography
+          variant="h7"
+        >
+          I'm a driver
+        </Typography>
+        </Button>
       </div>
-
-      {/* {groupsDisplay} */}
-
-      {/* should add a new group bc new driver on click */}
-      <Button 
-      variant="contained" 
-      size="large"
-      sx={{
-        color:"#F7F7F6", 
-        backgroundColor:"#77BB3F",
-        borderRadius: 10,
-        ':hover': {
-          backgroundColor: '#77BB3F',
-        }
-      }} 
-      className="btn"
-      >
-      <Typography
-        variant="h7"
-      >
-        I'm a driver
-      </Typography>
-      </Button>
-    </div>
-    </div>
-  )
-
+      </div>
+    )
+  }
    // console.log(this.props.match.params.id);  
 }
-
-export default EventPage;
 
