0919e7b61e54a5e79e0f1d02d1132acb6bdd6f8b
Allison Lu <allison.lu@gatech.edu>
2022-10-22 14:11:21 -0400

    Group as funtion

--- src/Components/Group.js
+++ src/Components/Group.js
@@ -3,61 +3,55 @@ import { Typography } from '@mui/material';
 import React, { Component } from 'react';
 import '../css/App.css';
 
-export default class Group extends Component {
-    constructor(props) {
-        super(props);
-       // this.handleMouseMove = this.handleMouseMove.bind(this);
-        this.state = { driver: "Driver", riders: [] };
-    }
+function Group() {
+    const ridersDisplay = this.state.riders.map((rider) =>
+        <p className="item">{rider}</p>
+    );
 
-    render() { 
-        const ridersDisplay = this.state.riders.map((rider) =>
-            <p className="item">{rider}</p>
-        );
-
-        return( 
-            <div className="box p3">
-                <h3 className="item">{this.state.driver}</h3>
-                {ridersDisplay}
-                <div className="item">
-                <Button 
-                    variant="contained" 
-                    size="small"
-                    sx={{
-                        color:"#F7F7F6", 
-                        backgroundColor:"#77BB3F",
-                        ':hover': {
-                        backgroundColor: '#77BB3F',
-                        }
-                    }} 
-                    className="btn"
-                    >
-                    <Typography
-                        variant="h7"
-                    >
-                        Edit
-                    </Typography>
-                </Button>
-                <Button 
-                    variant="contained" 
-                    size="small"
-                    sx={{
-                        color:"#F7F7F6", 
-                        backgroundColor:"#77BB3F",
-                        ':hover': {
-                        backgroundColor: '#77BB3F',
-                        }
-                    }} 
-                    className="btn"
-                    >
-                    <Typography
-                        variant="h7"
-                    >
-                        Join
-                    </Typography>
-                </Button>
-                </div>
+    return( 
+        <div className="box p3">
+            <h3 className="item">{this.state.driver}</h3>
+            {ridersDisplay}
+            <div className="item">
+            <Button 
+                variant="contained" 
+                size="small"
+                sx={{
+                    color:"#F7F7F6", 
+                    backgroundColor:"#77BB3F",
+                    ':hover': {
+                    backgroundColor: '#77BB3F',
+                    }
+                }} 
+                className="btn"
+                >
+                <Typography
+                    variant="h7"
+                >
+                    Edit
+                </Typography>
+            </Button>
+            <Button 
+                variant="contained" 
+                size="small"
+                sx={{
+                    color:"#F7F7F6", 
+                    backgroundColor:"#77BB3F",
+                    ':hover': {
+                    backgroundColor: '#77BB3F',
+                    }
+                }} 
+                className="btn"
+                >
+                <Typography
+                    variant="h7"
+                >
+                    Join
+                </Typography>
+            </Button>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+export default Group;
