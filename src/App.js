66b7ca989471ecb581f9aa9da5a333878d5f9220
Chase-rgb <62972320+Chase-rgb@users.noreply.github.com>
2022-10-22 01:37:07 -0400

    Add Chat.js and moved Firebase

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
 import StartPage from './Pages/StartPage.js'
+import Chat from './Pages/Chat.js';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<StartPage />} />
+        <Route exact path='/' element={<Chat />} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/Chat.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { db } from "../firebase/firebase"
+
+export default class Chat extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        chats: [],
+        content: '',
+        readError: null,
+        writeError: null
+      };
+    }
+    async componentDidMount() {
+      this.setState({ readError: null });
+      try {
+        db.ref("chats").on("value", snapshot => {
+          let chats = [];
+          snapshot.forEach((snap) => {
+            chats.push(snap.val());
+          });
+          this.setState({ chats });
+        });
+      } catch (error) {
+        this.setState({ readError: error.message });
+      }
+    }
+    render() {
+        return (
+          <div>
+            <div className="chats">
+              {this.state.chats.map(chat => {
+                return <p key={chat.timestamp}>{chat.content}</p>
+              })}
+            </div>
+            <div>
+              Login in as: <strong>{this.state.user.email}</strong>
+            </div>
+          </div>
+        );
+      }
+}
+  
+  
