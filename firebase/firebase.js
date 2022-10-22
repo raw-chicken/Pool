9142e810c916875bf1bc3b8dca0240e2a678e0b5
Jason Guo <jasonyguo@gmail.com>
2022-10-22 01:07:52 -0400

    firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,67 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4Omc4TL9_yt6qyhNZO5HQcn7v6B1vyFc",
+  authDomain: "pool-ff59e.firebaseapp.com",
+  databaseURL: "https://pool-ff59e-default-rtdb.firebaseio.com",
+  projectId: "pool-ff59e",
+  storageBucket: "pool-ff59e.appspot.com",
+  messagingSenderId: "72939662900",
+  appId: "1:72939662900:web:676e14f785890802de6461"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+
+import { getDatabase, ref, onValue, set, child, push, update } from "firebase/database";
+
+// Actively listen for database changes on new groups and such
+// groups gets info from firebase
+// updateGroups updates state which in turn adds component to page
+const groups = ref(db, 'event/' + eventID + '/groups');
+onValue(groups, (snapshot) => {
+  const data = snapshot.val();
+  updateGroups(data);
+});
+
+export function createEvent(desc) {
+  const eventID = -1 // some hashing algorithm
+  const db = getDatabase();
+  set(ref(db, 'event/' + eventID), {
+    description: desc
+  });
+}
+
+// export function writeNewPost(uid, username, picture, title, body) {
+//   const db = getDatabase();
+
+//   // A post entry.
+//   const postData = {
+//     author: username,
+//     uid: uid,
+//     body: body,
+//     title: title,
+//     starCount: 0,
+//     authorPic: picture
+//   };
+
+//   // Get a key for a new Post.
+//   const newPostKey = push(child(ref(db), 'posts')).key;
+
+//   // Write the new post's data simultaneously in the posts list and the user's post list.
+//   const updates = {};
+//   updates['/posts/' + newPostKey] = postData;
+//   updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+//   return update(ref(db), updates);
+// }
