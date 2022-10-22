9082a64279a6ea0071980482987cbcb0fc72f3ca
Allison Lu <allison.lu@gatech.edu>
2022-10-21 23:14:05 -0400

    front

---
+++ src/components/TheStart.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="page">
+    <img
+      alt="Pool logo"
+      class="logo"
+      src="@/assets/pool-logo.jpeg"
+      width="300"
+      height="300"
+    />
+    <h3>Let's get started!</h3>
+    <button>--></button>
+    </div>
+</template>
---
+++ src/views/StartedView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TheStart from "../components/TheStart.vue";
+</script>
+
+<template>
+  <main>
+    <TheStart />
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Started",
+  created() {
+    document.body.style.backgroundColor = "#77BB3F";
+  },
+};
+</script>
+
