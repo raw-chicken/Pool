afc41850f458f230f76a2627f23a658afbe22a18
Allison Lu <allison.lu@gatech.edu>
2022-10-22 21:28:10 -0400

    beautify chat

--- src/Pages/Chat.js
+++ src/Pages/Chat.js
@@ -1,10 +1,13 @@
 import '../css/Chat.css';
+import Header from "../Components/Header.js"
 
 import React, { Component } from "react";
 import { database, addMessage } from "../firebase/firebase"
 import { ref, onValue } from "firebase/database";
 import { useParams } from "react-router-dom";
 import { getUserName } from '../Global/UserInfo';
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
 
 
 function withParams(Component) {
@@ -72,7 +75,9 @@ class Chat extends Component {
     render() {
         return (
         <div>
-            "Chat"
+            <Header />
+            <div className="content">
+            <h1>Chat</h1>
             <div className="chat-area">
             {Object.entries(this.state.chats).map(([time, message]) => {
                 //TODO: Need to get the user's Name ID
@@ -87,8 +92,30 @@ class Chat extends Component {
             <form className="mx-3" onSubmit={this.handleSubmit}>
             <input type="text" className="form-control" name="content" onChange={this.handleChange} value={this.state.content} ></input>
             {this.state.error ? <p className="text-danger">{this.state.error}</p> : null}
-            <button type="submit" className="btn btn-submit px-5 mt-4">Send</button>
+            <Button 
+            type="submit"
+            variant="contained" 
+            size ="small"
+            sx={{
+            marginTop: 2,
+            marginBottom: 2,
+            color:"#F7F7F6", 
+            backgroundColor:"#77BB3F",
+            ':hover': {
+                backgroundColor: '#77BB3F',
+            },
+            borderRadius: 10,
+            }} 
+            className="btn btn-submit"
+        >
+            <Typography
+            variant="h7"
+            >
+            Send
+            </Typography>
+        </Button>
             </form>
+            </div>
         </div>
         );
     }
