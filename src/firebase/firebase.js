9df1f5b4252a29c6bdd9eac4a0ef7277fa39b855
Jason Guo <jasonyguo@gmail.com>
2022-11-02 18:38:50 -0400

    Consolidate firebase functions

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -23,7 +23,8 @@ const app = initializeApp(firebaseConfig);
 // Initialize Realtime Database and get a reference to the service
 export const database = getDatabase(app);
 
-/* Actions */
+
+/* Event Functions */
 
 // Given a description, create an event
 export function createEvent(name, desc, date, time) {
@@ -64,36 +65,6 @@ export async function getEvent(eventID, page) {
   return val;
 }
 
-export async function getGroupInfo(groupID, page) {
-  let val = "NOTHING TO SEE HERE";
-  await get(ref(database, 'groups/' + groupID)).then((snapshot) => {
-    if (snapshot.exists()) {
-      val = snapshot.val();
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
-  page.setState({
-    update: true,
-    id: groupID,
-    driver: val.driver,
-    capacity: val.capacity,
-    desc: val.desc,
-    plate: val.plates,
-    passengers: val.passengers,
-    count: Object.keys(val.passengers).length
-  });
-
-  return val;
-}
- 
-
-export function addPassenger(groupID, userName, userId) {
-  update(ref(database, 'groups/' + groupID + "/passengers"), {
-    [userId]: userName
-  });
-}
-
 // Edit event
 export function editEvent(eventID, name, desc, date, time) {
   // Write the new post's data simultaneously in the posts list and the user's post list.
@@ -105,6 +76,10 @@ export function editEvent(eventID, name, desc, date, time) {
 
   return update(ref(database), updates);
 }
+
+/* End Event Functions */
+
+/* Group Functions */
 
 export async function createGroup(driver, capacity, plates, desc, eventID) {
   const current = new Date();
@@ -136,10 +111,33 @@ export async function createGroup(driver, capacity, plates, desc, eventID) {
   return groupID;
 }
 
-export function updateParent(parent) {
-  parent.setState({
-    update: !parent.update,
+export function deleteGroup(eventID, groupID) {
+  remove(ref(database, 'events/' + eventID + '/groups/' + groupID));
+  remove(ref(database, 'groups/' + groupID));
+  remove(ref(database, 'chats/' + groupID));
+}
+
+export async function getGroupInfo(groupID, page) {
+  let val = "NOTHING TO SEE HERE";
+  await get(ref(database, 'groups/' + groupID)).then((snapshot) => {
+    if (snapshot.exists()) {
+      val = snapshot.val();
+    }
+  }).catch((error) => {
+    console.error(error);
   });
+  page.setState({
+    update: true,
+    id: groupID,
+    driver: val.driver,
+    capacity: val.capacity,
+    desc: val.desc,
+    plate: val.plates,
+    passengers: val.passengers,
+    count: Object.keys(val.passengers).length
+  });
+
+  return val;
 }
 
 export function editGroupMetadata(driver, maxCapacity, description, groupID) {
@@ -152,29 +150,20 @@ export function editGroupMetadata(driver, maxCapacity, description, groupID) {
   set(ref(database, 'groups/' + groupID), groupData);
 }
 
-export function deleteGroup(eventID, groupID) {
-  remove(ref(database, 'events/' + eventID + '/groups/' + groupID));
-  remove(ref(database, 'groups/' + groupID));
-  remove(ref(database, 'chats/' + groupID));
+export function addPassenger(groupID, userName, userId) {
+  update(ref(database, 'groups/' + groupID + "/passengers"), {
+    [userId]: userName
+  });
 }
 
-export function addMember(name, groupID) {
-  const current = new Date();
-  const curr_time = current.toLocaleTimeString("en-US");
-
-  const plain_text = curr_time + name + groupID;
-
-  const hash = crypto.MD5(plain_text).toString()
-  const memberID = hash.substring(0, 7);
-
-  set(ref(database, 'groups/' + groupID + '/passengers/' + memberID), name);
-
-  return memberID;
-}
-
-export function removeMember(memberID, groupID) {
+export function removePassenger(memberID, groupID) {
   remove(ref(database, 'groups/' + groupID + '/passengers/' + memberID));
 }
+
+
+/* End Group Functions */
+
+/* Chat Functions */
 
 export function addMessage(name, text, groupId) {
   const time = new Date()
@@ -195,3 +184,5 @@ export function mountChat(state) {
     }
     return "Mount failed";
 }
+
+/* End Chat Functions */
